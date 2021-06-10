0d253442f8f02960f66be51ee90f12915deb3f77
Jason <nyjasonlam@gmail.com>
2021-06-10 01:01:52 -0400

    added db.js pseudocode to handle indexeddb stuff

--- public/db.js
+++ public/db.js
@@ -1,1 +1,26 @@
 // IndexedDB stuff here
+
+// open database
+
+// onupgrade needed incase we need to update schema / database version
+
+// onerror handler
+
+// function to check database
+// open transaction
+// specify obj store
+// get all items from obj store
+// if getAll is successful
+// create a fetch post request to bulk add data to the backend db
+// if response is good then clear items from obj store
+
+// onsuccess handler
+// specify pointer to indexeddb
+// if navigator online then check database function
+
+// function to save to indexedDB
+// open transaction
+// specify store
+// add to store
+
+// event listener, when online check database function
